8354402d0dee11338fd221751cf05986d8024217
Jan Michal <jan.michal@email.cz>
2024-01-24 21:52:39 +0100

    added discord profile

--- src/main/java/com/engeto/ThymeleafEShop/ThymeleafEShopApplication.java
+++ src/main/java/com/engeto/ThymeleafEShop/ThymeleafEShopApplication.java
@@ -3,6 +3,8 @@ package com.engeto.ThymeleafEShop;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+// discord janm.0554
+
 @SpringBootApplication
 public class ThymeleafEShopApplication {
 
